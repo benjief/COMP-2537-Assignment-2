be65102169b0243910b3e584c82333279863d8a6
Benjie Friedman <benjiefriedman@gmail.com>
2021-05-08 13:53:42 -0700

    Coded GreenQuest user table.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Requires
 
 const express = require('express');
 const app = express();
@@ -5,59 +6,64 @@ const fs = require("fs");
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
-
+// Gets
 app.get('/', function (req, res) {
 
+  // Build the DB if it doesn't exist
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    multipleStatements: true
+  });
 
-    // Let's build the DB if it doesn't exist
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      multipleStatements: true
-    });
-
-    const createDBAndTables = `CREATE DATABASE IF NOT EXISTS test;
-        use test;
-        CREATE TABLE IF NOT EXISTS customer (
+  const createDBAndTables =
+    // Table of GreenQuest users
+    `CREATE DATABASE IF NOT EXISTS greenquest;
+      use greenquest;
+    CREATE TABLE IF NOT EXISTS user (
         ID int NOT NULL AUTO_INCREMENT,
-        name varchar(30),
-        email varchar(30),
-        PRIMARY KEY (ID));`;
+        first_name varchar(30) NOT NULL,
+        last_name varchar(50) NOT NULL,
+        email varchar(50),
+        user_type varchar(30) NOT NULL,
+        class varchar(20),
+        PRIMARY KEY (ID)
+        );`;
 
-    connection.connect();
-    connection.query(createDBAndTables, function (error, results, fields) {
-      if (error) {
-          throw error;
-      }
-      //console.log(results);
+  connection.connect();
+  connection.query(createDBAndTables, function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    //console.log(results);
 
-    });
-    connection.end();
+  });
+  connection.end();
 
-    let doc = fs.readFileSync('./index.html', "utf8");
-    res.send(doc);
+  let doc = fs.readFileSync('./index.html', "utf8");
+  res.send(doc);
 });
 
 
 app.get('/get-customers', function (req, res) {
 
-    let connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'test'
-    });
-    connection.connect();
-    connection.query('SELECT * FROM customer', function (error, results, fields) {
-        if (error) {
-            throw error;
-        }
-        console.log('Rows returned are: ', results);
-        res.send({ status: "success", rows: results });
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+  });
+  connection.connect();
+  connection.query('SELECT * FROM customer', function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    console.log('Rows returned are: ', results);
+    res.send({ status: "success", rows: results });
 
-    });
-    connection.end();
+  });
+  connection.end();
 
 
 });
@@ -70,82 +76,82 @@ app.use(bodyParser.json());
 
 // Notice that this is a 'POST'
 app.post('/add-customer', function (req, res) {
-      res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'application/json');
 
-      console.log("Name", req.body.name);
-      console.log("Email", req.body.email);
+  console.log("Name", req.body.name);
+  console.log("Email", req.body.email);
 
-      let connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'test'
-      });
-      connection.connect();
-      // TO PREVENT SQL INJECTION, DO THIS:
-      // (FROM https://www.npmjs.com/package/mysql#escaping-query-values)
-      connection.query('INSERT INTO customer (name, email) values (?, ?)',
-            [req.body.name, req.body.email],
-            function (error, results, fields) {
-        if (error) {
-            throw error;
-        }
-        //console.log('Rows returned are: ', results);
-        res.send({ status: "success", msg: "Recorded added." });
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+  });
+  connection.connect();
+  // TO PREVENT SQL INJECTION, DO THIS:
+  // (FROM https://www.npmjs.com/package/mysql#escaping-query-values)
+  connection.query('INSERT INTO customer (name, email) values (?, ?)',
+    [req.body.name, req.body.email],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      //console.log('Rows returned are: ', results);
+      res.send({ status: "success", msg: "Recorded added." });
 
-      });
-      connection.end();
+    });
+  connection.end();
 
 });
 
 // POST: we are changing stuff on the server!!!
 app.post('/delete-all-customers', function (req, res) {
-      res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'application/json');
 
-      let connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'test'
-      });
-      connection.connect();
-      // REALLY A DUMB THING TO DO, BUT JUST SHOWING YOU CAN
-      connection.query('DELETE FROM customer',
-            function (error, results, fields) {
-        if (error) {
-            throw error;
-        }
-        //console.log('Rows returned are: ', results);
-        res.send({ status: "success", msg: "Recorded all deleted." });
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+  });
+  connection.connect();
+  // REALLY A DUMB THING TO DO, BUT JUST SHOWING YOU CAN
+  connection.query('DELETE FROM customer',
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      //console.log('Rows returned are: ', results);
+      res.send({ status: "success", msg: "Recorded all deleted." });
 
-      });
-      connection.end();
+    });
+  connection.end();
 
 });
 
 // ANOTHER POST: we are changing stuff on the server!!!
 app.post('/update-customer', function (req, res) {
-      res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'application/json');
 
-      let connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'test'
-      });
-      connection.connect();
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+  });
+  connection.connect();
 
-      connection.query('UPDATE customer SET email = ? WHERE ID = ?',
-            [req.body.email, req.body.id],
-            function (error, results, fields) {
-        if (error) {
-            throw error;
-        }
-        //console.log('Rows returned are: ', results);
-        res.send({ status: "success", msg: "Recorded updated." });
+  connection.query('UPDATE customer SET email = ? WHERE ID = ?',
+    [req.body.email, req.body.id],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      //console.log('Rows returned are: ', results);
+      res.send({ status: "success", msg: "Recorded updated." });
 
-      });
-      connection.end();
+    });
+  connection.end();
 
 });
 
